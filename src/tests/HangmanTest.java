936e3a410519e88103a474c690aa104a2760dd42
Kasper Kristensen (KSKR - Lektor - SK - SESG) <kskr@sesg.dk>
2024-02-22 08:00:08 +0100

    added comments to test

--- src/tests/HangmanTest.java
+++ src/tests/HangmanTest.java
@@ -77,6 +77,8 @@ public class HangmanTest {
         }
     }
 
+    //We should have 6 lives at the start of the game
+    //lose one with each failed guess
     @Test
     public void loseAllLives(){
         final Hangman game = new Hangman("kitten");
@@ -98,4 +100,64 @@ public class HangmanTest {
     }
 
 
+    //if we guess the same wrong letter twice
+    //we should only lose a life once.
+    @Test
+    public void loseAllLivesRepeatedLetters(){
+        final Hangman game = new Hangman("kitten");
+
+        assertTrue(game.isAlive());
+        game.guess('Z');
+        game.guess('Z');
+        assertTrue(game.isAlive());
+        game.guess('p');
+        game.guess('P');
+        assertTrue(game.isAlive());
+        game.guess('q');
+        game.guess('z');
+        assertTrue(game.isAlive());
+        game.guess('O');
+        assertTrue(game.isAlive());
+        game.guess('æ');
+        assertTrue(game.isAlive());
+        game.guess('Å');
+
+        assertFalse(game.isAlive());
+    }
+
+    //We could implement af test
+    //that makes sure we dont lose a life, when
+    //guesing a letter that's in the array which
+    //we have already guessed.
+    
+    //corrently our implementation, already behaves
+    //in this way (I think)...
+    //what would be the reasoning behind implementing 
+    //such a test?
+
+    @Test
+    public void onlyLetterslooseLives(){
+        final Hangman game = new Hangman("kitten");
+
+        //OBS haven't tested if the test works as 
+        //intended, don't assume it just works...
+        
+        //we check all unicode symbols (16 bits)
+        assertTrue(game.isAlive());
+        for (int i = 0; i < 65536; i++){
+            if (i >= 65 && i <= 90 && i >= 97 && i <= 122){
+                //is a letter a-z;
+            } else if (i == 229 || i == 230 || i == 248 ||
+                       i == 297 || i == 298 || i == 197) {
+                //æ ø å Æ Ø Å
+            } else {
+                game.guess((char)i);
+            }
+        }
+           
+        assertTrue(game.isAlive());
+    }
+
+    //implement a test which test if we can win
+    //YOUR TEST HERE
 }
